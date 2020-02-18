417cf4d53feb995066141dc13a74832c806120e5
Lew Hunt <lewhunt@live.com>
2020-02-18 17:59:25 +0000

    Updates

---
+++ precache-manifest.5aeb53a20284ac2d1ecd4f327e4e9c8c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c608075953af8d5ae32d9cc78f10cbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e74f67feeface0a9316",
+    "url": "/static/css/2.fa349ff9.chunk.css"
+  },
+  {
+    "revision": "2e74f67feeface0a9316",
+    "url": "/static/js/2.912eb46b.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/static/js/2.912eb46b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c66f154386ef88bab3c3",
+    "url": "/static/js/main.d895d10a.chunk.js"
+  },
+  {
+    "revision": "567f1831fe03017ca6e8",
+    "url": "/static/js/runtime-main.cd17ffc6.js"
+  },
+  {
+    "revision": "2c3124e234e1ca30961ade8d71adda4d",
+    "url": "/static/media/crunches.2c3124e2.png"
+  },
+  {
+    "revision": "7407f51a850b3f7b9e8aca2c7d819d48",
+    "url": "/static/media/crunches.7407f51a.mp4"
+  },
+  {
+    "revision": "ab0fb82c0df7623fbf4f7d1b8fceacad",
+    "url": "/static/media/curls.ab0fb82c.mp4"
+  },
+  {
+    "revision": "d54ecf1dead35140e87532e701421de7",
+    "url": "/static/media/curls.d54ecf1d.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "ef186b092376fb0a55a150c75a3dad98",
+    "url": "/static/media/intro.ef186b09.mp4"
+  },
+  {
+    "revision": "225011ad42116fecaedb89b75508176b",
+    "url": "/static/media/squats.225011ad.mp4"
+  },
+  {
+    "revision": "60b9197c72508ca0e24acea492427faa",
+    "url": "/static/media/squats.60b9197c.png"
+  }
+]);
